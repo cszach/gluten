c50d21d710b053787d027f8025fe813393f88b7c
Zach <duong.nguyen@rochester.edu>
2024-07-06 02:30:10 -0400

    feat: :sparkles: Create an image module with a function to load PNGs.

---
+++ src/image.rs
@@ -0,0 +1,105 @@
+use png::DecodingError;
+use pyo3::exceptions::PyTypeError;
+use pyo3::prelude::*;
+use std::fmt;
+use std::fs::File;
+
+/// A bitmap image.
+#[pyclass]
+#[derive(Debug, Clone, Default, PartialEq, PartialOrd, Hash, Eq, Ord)]
+pub struct Image {
+    /// The width of the image.
+    #[pyo3(get)]
+    width: u32,
+    /// The height of the image.
+    #[pyo3(get)]
+    height: u32,
+    /// The data specifying the color sequence in 8-bit integers e.g. if there
+    /// are 4 color channels, the values in the data are R, G, B, A, R, G, B, A,
+    /// and so on.
+    #[pyo3(get)]
+    data: Vec<u8>,
+}
+
+unsafe impl Send for Image {}
+unsafe impl Sync for Image {}
+
+/// Contains possible errors during the image loading process.
+#[pyclass]
+#[derive(Debug, Clone, PartialEq, PartialOrd, Hash, Eq, Ord)]
+pub enum ImageLoadingError {
+    /// An I/O error while trying to open the file e.g. file does not exist.
+    IoError,
+    /// An error while trying to decode the file as an image e.g. the format is
+    /// corrupted.
+    DecodingError,
+}
+
+impl fmt::Display for ImageLoadingError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                ImageLoadingError::IoError => "IO error",
+                ImageLoadingError::DecodingError => "Decoding error",
+            }
+        )
+    }
+}
+
+impl Default for ImageLoadingError {
+    fn default() -> Self {
+        ImageLoadingError::IoError
+    }
+}
+
+impl From<std::io::Error> for ImageLoadingError {
+    fn from(_: std::io::Error) -> Self {
+        ImageLoadingError::IoError
+    }
+}
+
+impl From<DecodingError> for ImageLoadingError {
+    fn from(_: DecodingError) -> Self {
+        ImageLoadingError::DecodingError
+    }
+}
+
+impl From<ImageLoadingError> for PyErr {
+    fn from(rust_error: ImageLoadingError) -> Self {
+        match rust_error {
+            ImageLoadingError::IoError => PyTypeError::new_err("IO error while loading image"),
+            ImageLoadingError::DecodingError => {
+                PyTypeError::new_err("Decoding error while loading image")
+            }
+        }
+    }
+}
+
+#[pymethods]
+impl Image {
+    /// Load a PNG image at the given path. The path can be relative or
+    /// absolute.
+    #[staticmethod]
+    #[pyo3(signature = (path))]
+    pub fn load_png(path: &str) -> Result<Image, ImageLoadingError> {
+        let file = File::open(path)?;
+        let decoder = png::Decoder::new(file);
+        let mut reader = decoder.read_info()?;
+
+        let info = reader.info();
+        let width = info.width;
+        let height = info.height;
+
+        let mut buffer = vec![0; reader.output_buffer_size()];
+        let frame = reader.next_frame(&mut buffer)?;
+        let bytes = &buffer[..frame.buffer_size()];
+
+        Ok(Image {
+            width,
+            height,
+            data: bytes.to_vec(),
+        })
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,11 @@
 use pyo3::prelude::*;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
+pub mod image;
 
-/// A Python module implemented in Rust.
+/// A Python module for building LUTs, written in Rust.
 #[pymodule]
 fn lut_builder(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_class::<image::Image>()?;
+
     Ok(())
 }
