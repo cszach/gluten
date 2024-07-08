0fb37d3c1fed0033c7f15ba548bdb7fe1cdbc2d9
Zach <duong.nguyen@rochester.edu>
2024-07-08 09:49:56 -0400

    chore: :bulb: Add a crate-level comments and comments for crate modules.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,36 @@
+//! A crate for building, processing, and applying LUTs (lookup tables) used in
+//! color correction and color grading.
+//!
+//! A work in progress, gLUTen currently allows you to:
+//! - [x] Generate LUTs.
+//!
+//! ## Python interface
+//!
+//! gLUTen comes with a Python interface so you can integrate it with scripts
+//! for DCCs (Maya, Blender, etc.). Here is an example:
+//! ```python
+//! import gluten
+//!
+//! image = gluten.Image.load_png("edited.png")
+//! lut = gluten.Lut.build(image, title="Test")
+//! lut.save("lut.cube", gluten.LutFormat.CubeLut, 6, True, False)
+//! ```
+//!
+//! ## Supported file formats
+//!
+//! ### Images
+//!
+//! - PNG
+//!
+//! ### LUTs
+//!
+//! - Cube LUT
+
 use pyo3::prelude::*;
 
+/// Image struct and functions for loading and saving bitmap images.
 pub mod image;
+/// LUT struct and functions for building, processing, and applying LUTs.
 pub mod lut;
 
 #[doc(inline)]
